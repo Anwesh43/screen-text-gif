e7bc0e73616c98db9c89223878178de0bcb5519e
Anwesh43 <anweshthecool0@gmail.com>
2018-02-17 23:02:56 +0530

    adding test files

--- test.js
+++ test.js
@@ -1,2 +1,2 @@
 const createScreenTextGif = require('.')
-createScreenTextGif(400,400,['#2ecc71','#8e44ad','#16a085','#e74c3c','#e67e22'],['Hello world','Hi World','Tie World','More to Come','Hello there'],'test.gif')
+createScreenTextGif(400,400,['#2ecc71','#8e44ad','#16a085','#e74c3c','#e67e22'],['Thank You','for all your wishes','26 years in the world','Hopefully','More to come'],'test.gif')
