2ed11a0058e8e50554062bee28ad493c289bcf7e
Anwesh43 <anweshthecool0@gmail.com>
2018-02-17 22:31:13 +0530

    adding project files

---
+++ test.js
@@ -0,0 +1,2 @@
+const createScreenTextGif = require('.')
+createScreenTextGif(400,400,['#2ecc71','#8e44ad','#16a085','#e74c3c','#e67e22'],['Hello world','Hi World','Tie World','More to Come','Hello there'],'test.gif')
